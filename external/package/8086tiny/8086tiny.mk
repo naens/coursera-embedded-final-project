6d6b72fb1d1c56476bbd70b7c1cbd306546ddaed
Andrei <naens@gmx.com>
2026-02-08 21:55:02 +0100

    8086tiny: use new commit

--- external/package/8086tiny/8086tiny.mk
+++ external/package/8086tiny/8086tiny.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-8086TINY_VERSION = 'c79ca2a34d96931d55ef724c815b289d0767ae3a'
+8086TINY_VERSION = '9b19ccc23c16cc61e4495a05705af47b120338f0'
 8086TINY_SITE = 'git@github.com:naens/8086tiny.git'
 8086TINY_SITE_METHOD = git
-8086TINY_OVERRIDE_SRCDIR = /home/andrei/gproj/8086tiny
 
 define 8086TINY_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) no_graphics
