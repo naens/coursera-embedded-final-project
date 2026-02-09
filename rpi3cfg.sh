b0295867bc26f97817c555a5362d14a3b9e9f706
Andrei <naens@gmx.com>
2026-02-09 21:36:08 +0100

    add rpi3 script

--- rpi3cfg.sh
+++ rpi3cfg.sh
@@ -8,18 +8,29 @@ BUILDROOT=buildroot
 BUILDROOT_RPI3=buildroot_rpi3
 EXTERNAL=$(realpath external)
 RPI0_DEFCONFIG=${BUILDROOT}/configs/raspberrypi0_defconfig
-RPI3_DEFCONFIG=${BUILDROOT}/configs/raspberrypi3_defconfig
+RPI3_DEFCONFIG=${BUILDROOT_RPI3}/configs/raspberrypi3_defconfig
+
+pause() {
+  read -n 1 -s -p "$*"
+  echo ""
+}
 
 # save config from current buildroot
 make -C ${BUILDROOT} savedefconfig  BR2_DEFCONFIG=${TMP_DEFCONFIG}
+
+pause "defconfig saved into ${TMP_DEFCONFIG}"
 
 # diff with rpi0 defconfig
 diff --new-line-format='%L' --unchanged-line-format='' \
      --old-line-format='' ${RPI0_DEFCONFIG} ${TMP_DEFCONFIG} \
      > ${TMP_DIFF_CFG} || true
 
+pause "config diff in ${TMP_DIFF_CFG}"
+
 # merge defconfig with rpi3
 cat ${RPI3_DEFCONFIG} ${TMP_DIFF_CFG} > ${TMP_RPI3_DEFCONFIG}
+
+pause "merged defconfig for rpi3 in ${TMP_RPI3_DEFCONFIG}"
 
 # generate rpi3 config
 make -C ${BUILDROOT_RPI3} defconfig BR2_EXTERNAL=${EXTERNAL} \
