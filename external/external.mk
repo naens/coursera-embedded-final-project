ec204b4be3d2312ea6a3873d91d43305fd33edbc
Andrei <naens@gmx.com>
2026-02-09 21:38:25 +0100

    add kedei (temporary) driver

--- external/external.mk
+++ external/external.mk
@@ -1,3 +1,3 @@
 include $(BR2_EXTERNAL_project_base_PATH)/package/ccpm/ccpm.mk
 include $(BR2_EXTERNAL_project_base_PATH)/package/8086tiny/8086tiny.mk
-#include $(BR2_EXTERNAL_project_base_PATH)/package/kedei/kedei.mk
+include $(BR2_EXTERNAL_project_base_PATH)/package/kedei/kedei.mk
--- external/package/kedei/kedei.mk
+++ external/package/kedei/kedei.mk
@@ -4,12 +4,26 @@
 #
 ################################################################################
 
-KEDEI_VERSION = '968b9399f1fdcdedce1611bea53448dc5fbcad3f'
+KEDEI_VERSION = '28e7bc61b962b2970cee65b31537dabbbc9cff78'
 KEDEI_SITE = 'git@github.com:naens/FBTFT_KeDei_35_lcd_v62.git'
 KEDEI_SITE_METHOD = git
-#KEDEI_OVERRIDE_SRCDIR = ../kedei
+KEDEI_OVERRIDE_SRCDIR = $(TOPDIR)/../kedei
+KEDEI_INSTALL_IMAGES = YES
 
 KEDEI_MODULE_MAKE_OPTS=$(TARGET_CONFIGURE_OPTS)
 
 $(eval $(kernel-module))
+
+define KEDEI_BUILD_DT_OVERLAY
+	$(LINUX_DIR)/scripts/dtc/dtc -@ -I dts -O dtb \
+		-o $(@D)/kedei.dtbo $(@D)/kedei.dts
+endef
+
+KEDEI_POST_BUILD_HOOKS += KEDEI62_BUILD_DT_OVERLAY
+
+define KEDEI_INSTALL_IMAGES_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/kedei.dtbo \
+		$(BINARIES_DIR)/rpi-firmware/overlays/kedei.dtbo
+endef
+
 $(eval $(generic-package))
